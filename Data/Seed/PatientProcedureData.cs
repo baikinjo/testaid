aae81b4fd2cf1f984dfb753eb750e6efafa4e0ef
Injo Baik <baikinjo.28@gmail.com>
2016-12-05 18:25:54 -0800

    seeding
    Signed-off-by: Injo Baik <baikinjo.28@gmail.com>

---
+++ Data/Seed/PatientProcedureData.cs
@@ -0,0 +1,22 @@
+﻿using SecondAid.Models.Health;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondAid.Data.Seed
+{
+    public class PatientProcedureData
+    {
+        public static List<PatientProcedure> SeedData(ApplicationDbContext context)
+        {
+            List<PatientProcedure> items = new List<PatientProcedure>()
+            {
+
+            };
+
+
+            return items;
+        }
+    }
+}
---
+++ Data/Seed/ScheduleData.cs
@@ -0,0 +1,32 @@
+﻿using SecondAid.Models.Health;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondAid.Data.Seed
+{
+    public class ScheduleData
+    {
+        public static List<Schedule> SeedData(ApplicationDbContext context)
+        {
+            List<Schedule> items = new List<Schedule>() {
+                new Schedule {
+                    Patient = context.ApplicationUser.FirstOrDefault(s => s.UserName == "jane"),
+                    Procedure = context.Procedures.FirstOrDefault(s => s.Name == "Root Canal"),
+                    Time = Convert.ToDateTime("2016/12/04 8:30 am"),
+                },
+
+                new Schedule
+                {
+                    Patient = context.ApplicationUser.FirstOrDefault(s => s.UserName == "jane"),
+                    Procedure = context.Procedures.FirstOrDefault(s => s.Name == "Teeth Whitening"),
+                    Time = Convert.ToDateTime("2016/12/10 1:30 pm"),
+                }
+           };
+
+            return items;
+        }
+
+    }
+}
